00c5f3a0a67044c9a3a1312701844e97c49dd643
Ayush Singh <121350355+royalrajputayush@users.noreply.github.com>
2025-04-19 00:14:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import "../styles/globals.css";
 
 export const metadata = {
-  title: "TechExtreme-Home",
+  title: "SIT_TECH_KRITI-Home",
   description: "Events You Love!!",
 };
 
